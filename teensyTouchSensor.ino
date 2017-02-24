8945132c55e8c3df01a627b4ce461fff9c1856db
mjkoehle <mjkoehle@gmail.com>
2017-02-24 08:09:06 -0800

    lights only trigger in debug (dip==0)

--- teensyTouchSensor.ino
+++ teensyTouchSensor.ino
@@ -1,22 +1,21 @@
 /* Michael Koehle 01.30.2017
-Send json packet whenever state of one of the two touch sensors is changed. 
-Sensors are filtered using moving average. An initial calibration sets a threshold for
-each sensor. This is equal to sensorMax + thresholdScale * (sensorMax-sensorMin)
-ThresholdScale is a user set value
+  Send json packet whenever state of one of the two touch sensors is changed.
+  Sensors are filtered using moving average. An initial calibration sets a threshold for
+  each sensor. This is equal to sensorMax + thresholdScale * (sensorMax-sensorMin)
+  ThresholdScale is a user set value, 1 for each sensor. 1 is left. 2 ir right. 
 
- */
+*/
 
- // MK read dip. 
 
 /*user variables*/
-int sourceIP = 100; //set the ip for this teensy
-double thresholdScale = .3; // see above
+double thresholdScale1 = .4; // see above, threshold for button 1
+double thresholdScale2 = .4; // button 2
 int calTime = 5000;  //calibraton time on startup
 int beatDelay = 30000; //interval between sending beat packets, in millisenconds
 
 #include <SPI.h>
 #include <Ethernet.h>
-#include <ArduinoJson.h> //need arduinoJson library here: https://github.com/bblanchon/ArduinoJson
+//#include <ArduinoJson.h> //need arduinoJson library here: https://github.com/bblanchon/ArduinoJson
 #include "mac.h" //local import
 
 
@@ -28,13 +27,13 @@ int beatDelay = 30000; //interval between sending beat packets, in millisenconds
 /*network variables*/
 const unsigned int resetPin = 9; //Ethernet Reset Pin
 //extern uint8_t mac[6];
-byte macB[] = {0x04, 0xE9, 0xE5, 0x03, 0x5F, 0x44};
+//byte macB[] = {0x04, 0xE9, 0xE5, 0x03, 0x5F, 0x44};
 //04:E9:E5:03:5F:10 [Teensy A]
 //04:E9:E5:03:5F:44 [Teensy B]
 
-IPAddress localIp(192, 168, 0, sourceIP);
+//IPAddress localIp(192, 168, 0, sourceIP);
 IPAddress remoteIp(192, 168, 0 , 3);
-unsigned int serverPort = 3335; //change variable name
+unsigned int serverPort = 3333; /
 unsigned int localPort = 7777;
 EthernetUDP udp;
 IPAddress localCheck;
@@ -55,7 +54,7 @@ String choice1 = "left"; // color for pin1
 String choice2 = "right"; //color for pin2
 
 /*led variables*/
-int redPin= 19; //red is 19
+int redPin = 19; //red is 19
 int greenPin = 20; //green is 20
 int bluePin = 21;
 byte redValue = LOW;
@@ -73,12 +72,12 @@ double total2 = 0;
 double average2 = 0;
 
 int inputPin1 = A8;   //  touch sensor1
-int inputPin2 = A9;   // touch sensor2 
+int inputPin2 = A9;   // touch sensor2
 
 /* messaging logic variables*/
 int triggerTime1 = 0;
 int triggerTime2 = 0;
-int currentBeat = 0; 
+int currentBeat = 0;
 boolean trigger1 = false;
 boolean trigger2 = false;
 boolean sendMsg1 = false;
@@ -86,9 +85,12 @@ boolean sendMsg2 = false;
 double thresholds[2];
 double threshold1 = 0;
 double threshold2 = 0;
+boolean udpConnected = 0;
+
 
 void setup() {
-  
+  delay(1000); //wait to power up
+
   // initialize all the readings to 0:
   for (int thisReading = 0; thisReading < numReadings; thisReading++) {
     readings1[thisReading] = 0;
@@ -96,43 +98,43 @@ void setup() {
   }
 
   Serial.begin(38400);
-//  while (!Serial) {
-//    ; // wait for serial port to connect. Needed for native USB port only
-//  }
+  //  while (!Serial) {
+  //    ; // wait for serial port to connect. Needed for native USB port only
+  //  }
 
- 
+
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
-  pinMode(bluePin, OUTPUT); 
-  
+  pinMode(bluePin, OUTPUT);
+
   digitalWrite(redPin, 1);   // turn the LED on during calibration
-  digitalWrite(greenPin, 1);  
-  
+  digitalWrite(greenPin, 1);
+
   calibrate(&thresholds[0]); //calibrate
-  
+
   digitalWrite(redPin, 0);   // turn the LED off after calibration
-  digitalWrite(greenPin, 0);   
+  digitalWrite(greenPin, 0);
 
   //connection:
   currentModule = readDIPAddress();    //Read address from DIP
-  localIp = IPAddress(192, 168, 0, currentModule + 100); //101-103 are touch sensor. 104-1011 ceiling
+  IPAddress localIp = IPAddress(192, 168, 0, currentModule + 100); //101-103 are touch sensor. 104-1011 ceiling
   getMAC(); //assigns MAC address to variable mac
-   //Reset Ethernet Module
+  //Reset Ethernet Module
   resetEthernet();
   Ethernet.begin(mac, localIp);
-  udp.begin(localPort);
+  udpConnected = udp.begin(localPort);
   //client.connect(remoteIp, serverPort);  //MK this is for testing connection to a specific IP. Is there a check for just ethernet connection?
   //client.connected //.maintain
-
+  Serial.println(udpConnected);
   Serial.println(localIp);
   threshold1 = thresholds[0];
   threshold2 = thresholds[1];
   delay(1000);
-  
-  }
+
+}
 
 
-void loop() { 
+void loop() {
 
   /* Moving Average*/
   // subtract the last reading:
@@ -161,43 +163,48 @@ void loop() {
     if (trigger1 == false) { //if trigger1 is going from false to true, send msg to server
       sendMsg1 = true;
       redValue = HIGH;
+
+      triggerTime1 = triggerTime1 + 1;
+      trigger1 = true; //trigger1 is on
+      flavor1 = touchStart;
     }
-    triggerTime1 = triggerTime1 + 1;
-    trigger1 = true; //trigger1 is on
-    flavor1 = touchStart;
+
   }
   else {
     if (trigger1 == true) {//if trigger1 is going from true to false, send msg to server
       sendMsg1 = true;
       redValue = LOW;
+      triggerTime1 = 0;
+      trigger1 = false;
+      flavor1 = touchStop;
     }
-    triggerTime1 = 0;
-    trigger1 = false;
-    flavor1 = touchStop;
+
   }
-  
+
   sendMsg2 = false;
   if (average2 >= threshold2) {
     if (trigger2 == false) { //if trigger2 is going from false to true, send msg to server
       sendMsg2 = true;
       greenValue = HIGH;
+      triggerTime2 = triggerTime2 + 1;
+      trigger2 = true; //trigger1 is on
+      flavor2 = touchStart;
     }
-    triggerTime2 = triggerTime2 + 1;
-    trigger2 = true; //trigger1 is on
-    flavor2 = touchStart;
+
 
   }
   else {
     if (trigger2 == true) {
       sendMsg2 = true;
       greenValue = LOW;
+      triggerTime2 = 0;
+      trigger2 = false;
+      flavor2 = touchStop;
     }
-    triggerTime2 = 0;
-    trigger2 = false;
-    flavor2 = touchStop;
+
   }
-  
-if (millis()-currentBeat > beatDelay) {
+
+  if (millis() - currentBeat > beatDelay) {
     sendMsg1 = true;
     sendMsg2 = true;
     flavor1 = touchBeat;
@@ -210,7 +217,7 @@ if (millis()-currentBeat > beatDelay) {
   Serial.print("\t");
   Serial.print(average1);
   Serial.print("\t");
-   Serial.print(threshold1);
+  Serial.print(threshold1);
   Serial.print(touchRead(inputPin2));
   Serial.print("\t");
   Serial.print(average2);
@@ -220,45 +227,51 @@ if (millis()-currentBeat > beatDelay) {
   /* Json messaging */
   if (sendMsg1) {
 
-      /*send packet*/
-//      DynamicJsonBuffer jsonBuffer3;
-      char jsonChar1[100];
-      jsonString1 = "{\"source\": \"" + String(sourceIP) + "\",\"flavor\" :\""+ flavor1+"\",\"choice\" :\"" + choice1 + "\"}";
-      jsonString1.toCharArray(jsonChar1,100);
+    /*send packet*/
+    //      DynamicJsonBuffer jsonBuffer3;
+    char jsonChar1[100];
+    jsonString1 = "{\"source\": \"" + String(100+currentModule) + "\",\"flavor\" :\"" + flavor1 + "\",\"choice\" :\"" + choice1 + "\"}";
+    jsonString1.toCharArray(jsonChar1, 100);
+
+    if (currentModule > 0) { //if dip is set to 0, don't try to send
       udp.beginPacket(remoteIp, serverPort);
       udp.write(jsonChar1);
       udp.println();
       udp.endPacket();
-      
-      /*write color*/
-      digitalWrite(redPin, redValue);
-      digitalWrite(bluePin, blueValue);
-      digitalWrite(greenPin, greenValue);
-
+    }
+    else {  //else if dip (currentModule) is 0, change colors
+    /*write color*/
+    digitalWrite(redPin, redValue);
+    digitalWrite(bluePin, blueValue);
+    digitalWrite(greenPin, greenValue);
+    }
   }
-  
+
   if (sendMsg2) {
 
-      /*send packet*/
-      char jsonChar2[100];
-      jsonString2 = "{\"source\": \"" + String(sourceIP) + "\",\"flavor\" :\""+ flavor1+"\",\"choice\" :\"" + choice1 + "\"}";
-      jsonString2.toCharArray(jsonChar2,100);
+    /*send packet*/
+    char jsonChar2[100];
+    jsonString2 = "{\"source\": \"" + String(100+currentModule) + "\",\"flavor\" :\"" + flavor2 + "\",\"choice\" :\"" + choice2 + "\"}";
+    jsonString2.toCharArray(jsonChar2, 100);
+    
+    if (currentModule > 0) { //if dip is set to 0, don't try to send
       udp.beginPacket(remoteIp, serverPort);
       udp.write(jsonChar2);
       udp.println();
       udp.endPacket();
-
-      /*write color*/
-      digitalWrite(redPin, redValue);
-      digitalWrite(bluePin, blueValue);
-      digitalWrite(greenPin, greenValue);
-
+    }
+    else {  //else if dip (currentModule) is 0, change colors
+    /*write color*/
+    digitalWrite(redPin, redValue);
+    digitalWrite(bluePin, blueValue);
+    digitalWrite(greenPin, greenValue);
+     }
   }
 
   delay(1);
- } 
- /*end Loop*/
-  
+}
+/*end Loop*/
+
 
 
 
@@ -278,9 +291,9 @@ void calibrate(double *thresholds)
   int sensorMax2 = 0;           // maximum sensor value
   int touchMax2 = 0;
 
-  double idle1 = 0;  //touch calibration variable. idle is halfway between max and min during not touched 
+  double idle1 = 0;  //touch calibration variable. idle is halfway between max and min during not touched
   double idle2 = 0;
-  
+
   double threshold1 = 0;
   double threshold2 = 0;
 
@@ -288,9 +301,9 @@ void calibrate(double *thresholds)
   int counter = 0;
   int flagMe = 1;
   int debugOn = 1;
-  
+
   while (millis() < calTime + touchTime) {
-    counter = counter+1;
+    counter = counter + 1;
 
     /* Moving Average*/
     // subtract the last reading:
@@ -314,12 +327,12 @@ void calibrate(double *thresholds)
     average2 = total2 / numReadings;
 
     /*calculate min/maxes*/
-    if (counter > numReadings*2 && millis() < calTime) { //give it some for moving average to take affect
-      // record the maximum sensor value    
+    if (counter > numReadings * 2 && millis() < calTime) { //give it some for moving average to take affect
+      // record the maximum sensor value
       if ( average1 > sensorMax1) {
         sensorMax1 =  average1;
       }
-      
+
       // record the minimum sensor value
       if ( average1 < sensorMin1) {
         sensorMin1 =  average1;
@@ -354,10 +367,10 @@ void calibrate(double *thresholds)
   thresholds[1] = idle2 + .5 * (touchMax2 - idle2); //calculate a threshold for trigger
 
   /*method2: for calibration without touching sensor. */
-  thresholds[0] = sensorMax1 + thresholdScale * (sensorMax1-sensorMin1); //calculate a threshold for trigger
-  thresholds[1] = sensorMax2 + thresholdScale * (sensorMax2-sensorMin2); //calculate a threshold for trigger
+  thresholds[0] = sensorMax1 + thresholdScale1 * (sensorMax1 - sensorMin1); //calculate a threshold for trigger
+  thresholds[1] = sensorMax2 + thresholdScale2 * (sensorMax2 - sensorMin2); //calculate a threshold for trigger
 
-//  delay(1);
+  //  delay(1);
 
 }
 
